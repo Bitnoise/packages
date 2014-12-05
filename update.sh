33b59233c58b3c787ff5d2ed681c4135232871fd
Adam Misiorny <adam.misiorny@gmail.com>
2014-12-05 16:21:14 +0100

    update

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+GIT="$(which git)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GITDIR="$DIR/.git"
 
-echo $DIR
+# cd $DIR
 
-cd $DIR
-
-git pull
-vendor/bin/satis build
-git add .
-git commit -m "update"
-git push
+$GIT --git-dir=$GITDIR pull
+# $DIR/vendor/bin/satis build
+$GIT --git-dir=$GITDIR add $DIR
+$GIT --git-dir=$GITDIR commit -m "update"
+$GIT --git-dir=$GITDIR push
