e61f2d8dc6029f02be616c7b3dc65592460336c8
Adam Misiorny <adam.misiorny@gmail.com>
2014-12-05 16:49:11 +0100

    pimp update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,13 @@ GIT="$(which git)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GITDIR="$DIR/.git"
 
-# cd $DIR
+# pull latest changes from repo
+$GIT --git-dir=$GITDIR --work-tree=$DIR pull
 
-$GIT --git-dir=$GITDIR pull
-# $DIR/vendor/bin/satis build
-$GIT --git-dir=$GITDIR add $DIR
+# update packages
+$DIR/vendor/bin/satis build $DIR/satis.json $DIR
+
+# push changes to repo
+$GIT --git-dir=$GITDIR --work-tree=$DIR add --verbose .
 $GIT --git-dir=$GITDIR commit -m "update"
 $GIT --git-dir=$GITDIR push
